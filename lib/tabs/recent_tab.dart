698ddabc86dd6bfe0423a3ac13cc3167dfb405ad
Ritvik Singh <75081330+Ritviksingh-cyber@users.noreply.github.com>
2023-02-17 12:41:30 +0530

    Create recent_tab.dart

---
+++ lib/tabs/recent_tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:nft_app/components/nft_card.dart';
+
+class RecentTab extends StatelessWidget {
+  const RecentTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const NftCard(
+      imagePath: 'lib/images/4.png',
+
+    );
+
+  }
+}
