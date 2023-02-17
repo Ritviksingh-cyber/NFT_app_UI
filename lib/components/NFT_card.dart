57385dfc84b4eb5560cf50dc620b646c33524d83
Ritvik Singh <75081330+Ritviksingh-cyber@users.noreply.github.com>
2023-02-17 12:37:22 +0530

    Create NFT_card.dart

---
+++ lib/components/NFT_card.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class NftCard extends StatelessWidget {
+  final String imagePath;
+
+  const NftCard({
+    super.key,
+    required this.imagePath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:const EdgeInsets.all(8.0),
+      child: ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child:  Image.asset(imagePath,
+      fit: BoxFit.fill,
+    ),
+
+    ));
+  }
+}
